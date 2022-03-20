4ac8d2a8a19b1ae417618e84fd16820a5b46d017
Sharonchep <sharon30@gmail.com>
2022-03-21 01:27:45 +0300

    add new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,94 @@
-$(document).ready(function() {
-  $(".click").click(function() {
-    $(".clicked").show();
-    $(".click").hide();
-    $("#clickable").click(function() {
-    $(".click").show();
-    $(".clicked").hide();
-  });
-});
-$(".good").click(function() {
-  $(".best").show();
-  $(".good").hide();
-  $("#better").click(function() {
-  $(".good").show();
-  $(".best").hide();
-});
-});
-$(".bad").click(function() {
-  $(".worst").show();
-  $(".bad").hide();
-  $("#worse").click(function() {
-  $(".bad").show();
-  $(".worst").hide();
-});
-});
-$("#first").hover(function(){
-  $(".text-block1").toggle();
-  });
-$("#second").hover(function(){
-  $(".text-block2").toggle();
-  });
-$("#third").hover(function(){
-  $(".text-block3").toggle();
-  });
-$("#forth").hover(function(){
-  $(".text-block4").toggle();
-  });
-$("#fifth").hover(function(){
-  $(".text-block5").toggle();
-  });
-$("#sixth").hover(function(){
-  $(".text-block6").toggle();
-  });
-$("#seventh").hover(function(){
-  $(".text-block7").toggle();
-  });
-$("#eighth").hover(function(){
-  $(".text-block8").toggle();
-  }); 
+import { foodItem } from "./fooditem.js";
 
-$('#lost').submit(function(e){
-  e.preventDefault();
-});
-$('button').click(function(e){
- $("#lost")[0].reset();
-});
-});
+console.log(foodItem);
+
+function displayItems (){
+    var cripsy= document.getElementById('cripsy');
+    var stuffed= document.getElementById('stuffed');
+    var glutenFree= document.getElementById('glutenfree');
+    var toppinggs= document.getElementById('toppings');
+    
+    
+    const cripsyData= foodItem.filter(item=> item.category== 'cripsy' );
+    console.log(cripsyData);
+    const stuffedData= foodItem.filter(item=> item.category== 'stuffed' );
+    console.log(stuffedData);
+    const glutenFreeData= foodItem.filter(item=> item.category== 'glutenfree' );
+    console.log(glutenFreeData);
+    const toppingsData= foodItem.filter(item=> item.category== 'toppings' );
+    console.log(glutenFreeData);
+
+    cripsyData.map(item=> {
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card');
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top'); 
+        
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ''+ item.rating;
+        
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart add-to-cart');
+        heart.setAttribute('id',item.id);
+       
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img= document.createElement('img');
+        img.src= item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText=item.name;
+        
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $' + item.price;
+        
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        cripsy.appendChild(itemCard);
+    })  
+    stuffedData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card');
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top'); 
+        
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ''+ item.rating;
+        
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart add-to-cart');
+        heart.setAttribute('id',item.id);
+       
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img= document.createElement('img');
+        img.src= item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText=item.name;
+        
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $' + item.price;
+        
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        stuffed.appendChild(itemCard);
+    }) 
