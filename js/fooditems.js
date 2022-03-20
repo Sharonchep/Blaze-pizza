a8dbcb7ac9782ec2452b11dfd17b18b97f60852f
Sharonchep <sharon30@gmail.com>
2022-03-21 01:06:12 +0300

    add new changes

--- js/fooditems.js
+++ js/fooditems.js
@@ -17,5 +17,32 @@ const foodItem=[
         img:'img/image6.jpeg',
         quantity:1
     },
+    {
+        id:3,
+        name:'Cripsy large',
+        category:'cripsy',
+        rating:4-3,
+        price:8.8,
+        img:'img/image6.jpeg',
+        quantity:1
+    },
+    {
+        id:4,
+        name:'Stuffed small',
+        category:'stuffed',
+        rating:4.3,
+        price:8,
+        img:'img/pepperoni.jpg',
+        quantity:1
+    },
+    {
+        id:5,
+        name:'Stuffed medium',
+        category:'stuffed',
+        rating:4.3,
+        price:8.6,
+        img:'img/pepperoni.jpg',
+        quantity:1
+    },
 ]
     export { foodItem };
