be962b2de64d40685538425f8ac346ac132c38b2
Sharonchep <sharon30@gmail.com>
2022-03-21 01:01:50 +0300

    add new changes

---
+++ js/fooditems.js
@@ -0,0 +1,21 @@
+const foodItem=[
+    {
+        id:1,
+        name:'Cripsy small',
+        category:'cripsy',
+        rating:4.3,
+        price:7.5,
+        img:'img/image6.jpeg',
+        quantity:1
+    },
+    {
+        id:2,
+        name:'Cripsy medium',
+        category:'cripsy',
+        rating:4.3,
+        price:8.6,
+        img:'img/image6.jpeg',
+        quantity:1
+    },
+]
+    export { foodItem };
---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  $(".click").click(function() {
+    $(".clicked").show();
+    $(".click").hide();
+    $("#clickable").click(function() {
+    $(".click").show();
+    $(".clicked").hide();
+  });
+});
+$(".good").click(function() {
+  $(".best").show();
+  $(".good").hide();
+  $("#better").click(function() {
+  $(".good").show();
+  $(".best").hide();
+});
+});
+$(".bad").click(function() {
+  $(".worst").show();
+  $(".bad").hide();
+  $("#worse").click(function() {
+  $(".bad").show();
+  $(".worst").hide();
+});
+});
+$("#first").hover(function(){
+  $(".text-block1").toggle();
+  });
+$("#second").hover(function(){
+  $(".text-block2").toggle();
+  });
+$("#third").hover(function(){
+  $(".text-block3").toggle();
+  });
+$("#forth").hover(function(){
+  $(".text-block4").toggle();
+  });
+$("#fifth").hover(function(){
+  $(".text-block5").toggle();
+  });
+$("#sixth").hover(function(){
+  $(".text-block6").toggle();
+  });
+$("#seventh").hover(function(){
+  $(".text-block7").toggle();
+  });
+$("#eighth").hover(function(){
+  $(".text-block8").toggle();
+  }); 
+
+$('#lost').submit(function(e){
+  e.preventDefault();
+});
+$('button').click(function(e){
+ $("#lost")[0].reset();
+});
+});
